b05367980bc0698c68d99f97a58174b6a98ebc5b
juangpu <juangabriel.urrutia@outlook.com>
2020-08-24 18:45:41 -0300

    Adicionando barra de menu com bootstrap e CSS para todas as paginas (users, cadastro, dados do usaurio e editar perfil)

--- ProjetoDB-I/routes/users.js
+++ ProjetoDB-I/routes/users.js
@@ -6,10 +6,10 @@ const userController = require('../controllers/userController');
 router.get('/', userController.index);
 router.get('/ver/:id', userController.findById);
 router.get('/search', userController.search);
-router.get('/agregacoes', userController.agregadores);
+router.get('/agregacoes', userController.agregadores); // teste
 router.get('/cadastro', userController.create);
 router.post('/cadastro', userController.store);
-router.get('/criacao', userController.bulkCreate);
+router.get('/criacao', userController.bulkCreate); // teste
 router.get('/editar/:id', userController.edit);
 router.put('/editar/:id', userController.update);
 router.delete('/deletar/:id', userController.destroy);
