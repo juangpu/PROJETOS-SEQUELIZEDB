96d828059c12ba450556ee1e050fc2224a16a421
juangpu <juangabriel.urrutia@outlook.com>
2020-10-10 00:43:21 -0300

    Usando como referencia para o P.I,

--- ProjetoDB-I/routes/users.js
+++ ProjetoDB-I/routes/users.js
@@ -7,8 +7,10 @@ router.get('/', userController.index);
 router.get('/ver/:id', userController.findById);
 router.get('/search', userController.search);
 router.get('/agregacoes', userController.agregadores); // teste
+
 router.get('/cadastro', userController.create);
 router.post('/cadastro', userController.store);
+
 router.get('/criacao', userController.bulkCreate); // teste
 router.get('/editar/:id', userController.edit);
 router.put('/editar/:id', userController.update);
